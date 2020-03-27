75a22ec1b04a9590c75f7eda0549f70fc5f01084
Daniel Jentsch <djentsch@team.mobile.de>
2020-03-27 14:53:34 +0100

    Remove solution and add maven

---
+++ src/test/java/com/cleancoder/args/ArgsTest.java
@@ -0,0 +1,14 @@
+package com.cleancoder.args;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class ArgsTest {
+
+  @Test
+  public void testCreateWithNoSchemaOrArguments() {
+    assertThat(0).isEqualTo(0);
+  }
+}
